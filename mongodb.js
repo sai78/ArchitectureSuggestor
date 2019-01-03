ae08a5436c93ec4fa78a09588a7dab8bfc79937e
Balusupati Saibrahma <balusupatis@maqsoftware.com>
2019-01-03 17:54:38 +0530

    added code for updating documents

--- mongodb.js
+++ mongodb.js
@@ -3,40 +3,67 @@ const url = 'mongodb://localhost:27017/';
 
 const insertModule = (() => {
 
+    let counter = 1 , insertCounter1 = 1 , insertCounter2 = 1 , insertCounter3 = 1 ;
     const insertDataIntoDB = (questionEntityMappingObj, questionDetailsObj, architectureDetailsObj) => {
+        let myobj = questionEntityMappingObj;
+        let myobj1 = questionDetailsObj;
+        let myobj2 = architectureDetailsObj;
         mongodb.connect(url, { useNewUrlParser: true }, function (err, db) {
             if (err) throw err;
-            let dbo = db.db("Azure");
-            let myobj = questionEntityMappingObj;
-            let myobj1 = questionDetailsObj;
-            let myobj2 = architectureDetailsObj;
-            if (dbo.collection("questionEntityMappingCollection").count() == 0) {
+            let dbo = db.db("AzureTemplate");
+            
+            if(counter == 1)
+            {
+            dbo.createCollection("questionEntityMappingCollection");
+            dbo.createCollection("questionDetailsCollection");
+            dbo.createCollection("architectureDetailsCollection");
+            counter += 1;
+            }
+
+            if(insertCounter1 == 1)
+              {
                 dbo.collection("questionEntityMappingCollection").insertOne(myobj, function (err, res) {
                     if (err) throw err;
                     console.log("questionEntityMappingCollection document inserted");
                 });
+                insertCounter1 += 1;
             }
             else {
-                dbo.collection.save();
+                dbo.collection("questionEntityMappingCollection").remove();
+                dbo.collection("questionEntityMappingCollection").insertOne(myobj, function (err, res) {
+                    if (err) throw err;
+                    console.log("questionEntityMappingCollection document Updated");
+                });
             }
-            if (dbo.collection("questionDetailsCollection").count() == 0) {
+            if (insertCounter2 == 1) {
                 dbo.collection("questionDetailsCollection").insertOne(myobj1, function (err, res) {
                     if (err) throw err;
                     console.log("questionDetailsCollection document inserted");
                 });
+                insertCounter2 += 1;
             }
             else {
-                dbo.collection.save();
+                dbo.collection("questionDetailsCollection").remove();
+                dbo.collection("questionDetailsCollection").insertOne(myobj1, function (err, res) {
+                    if (err) throw err;
+                    console.log("questionDetailsCollection document Updated");
+                });
             }
-            if (dbo.collection("architectureDetailsCollection").count() == 0) {
+            if (insertCounter3 == 1) {
                 dbo.collection("architectureDetailsCollection").insertOne(myobj2, function (err, res) {
                     if (err) throw err;
                     console.log("architectureDetailsCollection document inserted");
                     db.close();
                 });
+                insertCounter3 += 1; 
             }
             else {
-                dbo.collection.save();
+                dbo.collection("architectureDetailsCollection").remove();
+                dbo.collection("architectureDetailsCollection").insertOne(myobj2, function (err, res) {
+                    if (err) throw err;
+                    console.log("architectureDetailsCollection document Updated");
+                    db.close();
+                });
             }
         });
     }
